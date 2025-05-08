68b49fa3dc51fe30f96b64455f2a717921580c83
ohziiilov3 <klbaskerville0520@gmail.com>
2025-05-08 10:08:53 -0700

    project setup and test apiKey to retreive data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+ // After
 const App = () => {
-  <div className="App">
-    
-  </div>
+  const [movies, setMovies] = useState([]);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  console.log("API Key:", apiKey);
+
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
+        const data = await res.json();
+        console.log(data)
+        setMovies(data);
+      } catch (error) {
+        console.log("Failed to fetch movies", error)
+      }
+    }
+    fetchMovies();
+  },[apiKey])
+
+
+
+
+  return (
+    <div className="App">
+      <h1>Movies</h1>
+
+    </div>
+  )
 }
 
 export default App
