9f506c270726c232e59730787d04c21bc24926f5
ohziiilov3 <klbaskerville0520@gmail.com>
2025-05-08 20:41:16 -0700

    create bonus  feature branch

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import "../styles/MovieCard.css"
-
-
+import { AiFillStar } from "react-icons/ai";  
+import { MdThumbUp } from "react-icons/md";
 
 const MovieCard = ({movie, onClick}) => {
     const imageBaseUrl = "https://image.tmdb.org/t/p/w500"
@@ -9,8 +9,12 @@ const MovieCard = ({movie, onClick}) => {
      <img src={`${imageBaseUrl}${movie.poster_path}`}alt={movie.title} className="movie-poster" />
     <div className="movie-info">
         <h3 className="movie-title">{movie.title}</h3>
-        <p> Votes: {movie.vote_count}</p>
-        <p className="movie-rating">⭐ {movie.vote_average}</p>
+        <p>
+          <MdThumbUp /> Votes: {movie.vote_count}
+        </p>
+        <p className="movie-rating" >
+          <AiFillStar className="star-icon"  /> Rating: {movie.vote_average}
+        </p>
     </div>
     </div> 
     );
